9a1876297a46706fc201ca47177c281a49178353
wzsamuel <wzsamuel@ncsu.edu>
2021-06-21 19:40:43 -0400

    Added options for sorting by movie data, selecting sorting algorithms and data structures.

--- FilmterWPF/MainWindow.xaml.cs
+++ FilmterWPF/MainWindow.xaml.cs
@@ -18,18 +18,18 @@ using DataStructures.List;
 using DataStructures.Hashing;
 using DataStructures.Map;
 using System.Collections.ObjectModel;
+using DataStructures.Sorter;
 
 namespace FilmterWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>     
+
     public partial class MainWindow : Window
     {
-        private SinglyLinkedList<Movie> movies;
-        private string path = "../../../data.tsv";
-        private IMap<string, Movie> movieMap;
-        private SkipListMap<string, Movie> skipMap;
+        public ObservableCollection<BasicMovie> MovieCollection { get; set; }
+        private string path = "../../../basicdata.tsv";
 
         public MainWindow()
         {
@@ -41,17 +41,92 @@ namespace FilmterWPF
             ProgressBar pb = new(path);
 
             bool? result = pb.ShowDialog();
-            
+
+            if (result == true)
+            {
+
+                MovieCollection = new(pb.MovieList);
+                dataGrid.DataContext = MovieCollection;
+            }
+            else
+            {
+                _ = MessageBox.Show("Error loading movies.");
+            }
+
+        }
+
+        private void Filter_Click(object sender, RoutedEventArgs e)
+        {
+            SortInfo sortInfo;
+            sortInfo.ascending = (bool)ascendingRadio.IsChecked;
+
+            if(linearHashRadio.IsChecked == true)
+            {
+                sortInfo.dataType = DataType.LinearHash;
+            }
+            else
+            {
+                sortInfo.dataType = 0;
+            }
+
+            if(bubbleRadio.IsChecked == true)
+            {
+                sortInfo.sortingAlgorithm = SortingAlgorithm.BubbleSort;
+            }
+            else 
+            {
+                sortInfo.sortingAlgorithm = 0;
+            }
+
+            if(yearRadio.IsChecked == true)
+            {
+                sortInfo.sortBy = SortBy.Year;
+            }
+            else
+            {
+                sortInfo.sortBy = 0;
+            }
+
+            SortProgress window = new(sortInfo, MovieCollection);
+
+            bool? result = window.ShowDialog();
+
             if(result == true)
             {
-                movieMap = pb.MovieMap;
-
-                ObservableCollection<Movie> data = new(pb.MovieList);
-                dataGrid.DataContext = data;
-
-
+                
+                    dataGrid.DataContext = window.MovieList;
+            }
+            else 
+            {
+                dataGrid.DataContext = null;
             }
             
+        }
+
+        public struct SortInfo
+        {
+            public SortBy sortBy;
+            public SortingAlgorithm sortingAlgorithm;
+            public bool ascending;
+            public DataType dataType;
+        }
+
+        public enum SortBy
+        {
+            Year,
+            Title,
+            RunTimeMinutes,
+            Genres
+        }
+
+        public enum DataType
+        {
+            LinearHash
+        }
+
+        public enum SortingAlgorithm
+        {
+            BubbleSort
         }
     }
 }
--- FilmterWPF/ProgressBar.xaml.cs
+++ FilmterWPF/ProgressBar.xaml.cs
@@ -27,12 +27,14 @@ namespace FilmterWPF
         private string path;
         private BackgroundWorker worker;
 
-        public SinglyLinkedList<Movie> MovieList { get; set; }
-        public SeparateChainingHashMap<string, Movie> MovieMap { get; set; }
+        public SinglyLinkedList<BasicMovie> MovieList { get; set; }
+        public SeparateChainingHashMap<string, BasicMovie> MovieMap { get; set; }
 
         public ProgressBar(string path)
         {
             this.path = path;
+            MovieMap = new();
+
             InitializeComponent();
 
             OkButton.IsEnabled = false;
@@ -61,14 +63,14 @@ namespace FilmterWPF
             CancelButton.IsEnabled = false;
             OkButton.IsEnabled = true;
 
-            if(e.Cancelled)
+            if (e.Cancelled)
             {
                 DialogResult = false;
             }
             else
             {
                 DialogResult = true;
-                MovieList = (SinglyLinkedList<Movie>)e.Result;
+                MovieList = (SinglyLinkedList<BasicMovie>)e.Result;
             }
         }
 
@@ -88,18 +90,87 @@ namespace FilmterWPF
         private void worker_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
-            e.Result = ReadMovieFile(worker, e);
+            e.Result = ReadBasicMovieFile(e);
         }
 
-        public SinglyLinkedList<Movie> ReadMovieFile(BackgroundWorker worker, DoWorkEventArgs e)
+        public SinglyLinkedList<BasicMovie> ReadBasicMovieFile(DoWorkEventArgs e)
+        {
+            bool firstLine = true;
+            SinglyLinkedList<BasicMovie> movieList = new();
+
+            //int totalLines = TotalLines();
+            int totalLines = 10000;
+            int currentLine = 0;
+
+            using (StreamReader reader = File.OpenText(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null && currentLine < totalLines)
+                {
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        string[] values = line.Split('\t');
+                        string id = values[0];
+                        string title = values[2];
+
+                        int? year;
+                        if (Int32.TryParse(values[3], out int temp))
+                        {
+                            year = temp;
+                        }
+                        else
+                        {
+                            year = null;
+                        }
+
+                        int? runTimeMinutes;
+                        if (int.TryParse(values[4], out temp))
+                        {
+                            runTimeMinutes = temp;
+                        }
+                        else
+                        {
+                            runTimeMinutes = null;
+                        }
+
+                        string genres = values[5];
+
+                        BasicMovie record = new(id, title, year, runTimeMinutes, genres);
+
+                        movieList.AddLast(record);
+                        MovieMap.Put(record.Id, record);
+
+                        currentLine++;
+                        int percentComplete = (int)((float)currentLine / (float)totalLines * 100);
+                        worker.ReportProgress(percentComplete);
+
+                        if (worker.CancellationPending)
+                        {
+                            e.Cancel = true;
+
+                            return null;
+                        }
+                    }
+                }
+            }
+            worker.ReportProgress(100);
+            return movieList;
+        }
+
+        /*
+        public SinglyLinkedList<Movie> ReadFullMovieFile(DoWorkEventArgs e)
         {
             SinglyLinkedList<Movie> movieList = new();
             MovieMap = new();
             bool firstLine = true;
 
-            //int totalLines = TotalLines();
-            int totalLines = 10000;
-            int currentLine = 0;
+            int totalLines = TotalLines();
+            //int totalLines = 10000;
+            int currentLine = 0;            
 
             using (StreamReader reader = File.OpenText(path))
             {
@@ -153,7 +224,7 @@ namespace FilmterWPF
                         Movie record = new(id, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runTimeMinutes, genres);
 
                         movieList.AddLast(record);
-                        MovieMap.Put(record.Id, record);
+                        //MovieMap.Put(record.Id, record);
                         
                         currentLine++;
                         int percentComplete = (int)((float)currentLine / (float)totalLines * 100);
@@ -170,8 +241,23 @@ namespace FilmterWPF
             }
             worker.ReportProgress(100);
 
+            using (StreamWriter writer = new("newdata.txt"))
+            {
+                foreach (Movie m in movieList)
+                {
+                    if (m.TitleType == "movie")
+                    {
+                        string line = m.Id + "\t" + m.TitleType + "\t" + m.PrimaryTitle + "\t" + m.StartYear + "\t" +
+                            m.RuntimeMinutes + "\t" + m.Genres;
+
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+
             return movieList;
         }
+        */
 
         private void Ok_Button_Click(object sender, RoutedEventArgs e)
         {
