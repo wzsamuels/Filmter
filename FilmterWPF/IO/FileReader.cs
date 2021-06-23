5f039c3e1fe510cf4d4c53949aafccd04b238a6e
Zach Samuels <wzsamuels@gmail.com>
2021-06-23 12:38:50 -0400

    Moved movie file data reading from ProgressBar class to FileReader class in order to increase separation between View and backend.

--- FilmterWPF/IO/FileReader.cs
+++ FilmterWPF/IO/FileReader.cs
@@ -2,30 +2,109 @@
 using System;
 using System.IO;
 using DataStructures.List;
+using System.ComponentModel;
 
 namespace FilmterWPF.IO
 {
     public static class FileReader
     {
-        private static int TotalLines(string filePath)
+        /// <summary>
+        /// Helper method to determine the total number of entries in the movie data file.
+        /// Used for the percentage complete on the progress bar.
+        /// </summary>
+        /// <returns>The number of entries in the data file.</returns>
+        private static int TotalEntriesHelper(string filePath)
         {
-            using (StreamReader r = new(filePath))
-            {
-                int i = 0;
-                while (r.ReadLine() != null) { i++; }
-                return i;
-            }
+            using StreamReader r = new(filePath);
+            int i = 0;
+            while (r.ReadLine() != null) { i++; }
+            return i;
         }
 
-        public static SinglyLinkedList<Movie> ReadMovieFile(string path)
+        public static SinglyLinkedList<BasicMovie> ReadBasicMovieFile(string path, DoWorkEventArgs e, BackgroundWorker worker)
         {
-            SinglyLinkedList<Movie> movieList = new();
             bool firstLine = true;
+            SinglyLinkedList<BasicMovie> movieList = new();
+
+            int totalLines = TotalEntriesHelper(path);
+            //int totalLines = 100000;
+            int currentLine = 0;
 
             using (StreamReader reader = File.OpenText(path))
             {
                 string line;
-                while ((line = reader.ReadLine()) != null)
+                while ((line = reader.ReadLine()) != null && currentLine < totalLines)
+                {
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        string[] values = line.Split('\t');
+                        string id = values[0];
+                        string title = values[2];
+
+                        // Convert string year to int. Skip over entries with no value.
+                        int? year;
+                        if (Int32.TryParse(values[3], out int temp))
+                        {
+                            year = temp;
+                        }
+                        else
+                        {
+                            //year = null;
+                            continue;
+                        }
+
+                        // Convert string run time minutes to int. Skip over entries with no value.
+                        int? runTimeMinutes;
+                        if (int.TryParse(values[4], out temp))
+                        {
+                            runTimeMinutes = temp;
+                        }
+                        else
+                        {
+                            //runTimeMinutes = null;
+                            continue;
+                        }
+
+                        string genres = values[5];
+
+                        BasicMovie record = new(id, title, year, runTimeMinutes, genres);
+
+                        movieList.AddLast(record);
+
+                        currentLine++;
+                        int percentComplete = (int)((float)currentLine / (float)totalLines * 100);
+                        worker.ReportProgress(percentComplete);
+
+                        if (worker.CancellationPending)
+                        {
+                            e.Cancel = true;
+
+                            return null;
+                        }
+                    }
+                }
+            }
+            worker.ReportProgress(100);
+            return movieList;
+        }
+
+        public static SinglyLinkedList<Movie> ReadFullMovieFile(string path, DoWorkEventArgs e, BackgroundWorker worker)
+        {
+            SinglyLinkedList<Movie> movieList = new();
+            bool firstLine = true;
+
+            int totalLines = TotalEntriesHelper(path);
+            //int totalLines = 10000;
+            int currentLine = 0;
+
+            using (StreamReader reader = File.OpenText(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null && currentLine < totalLines)
                 {
                     if (firstLine)
                     {
@@ -40,7 +119,7 @@ namespace FilmterWPF.IO
                         string originalTitle = values[3];
                         bool isAdult = (values[4] == "1");
                         int? startYear;
-                        if(Int32.TryParse(values[5], out int temp))
+                        if (Int32.TryParse(values[5], out int temp))
                         {
                             startYear = temp;
                         }
@@ -68,16 +147,42 @@ namespace FilmterWPF.IO
                         {
                             runTimeMinutes = null;
                         }
-                        
+
                         string genres = values[8];
 
                         Movie record = new(id, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runTimeMinutes, genres);
 
                         movieList.AddLast(record);
+                        //MovieMap.Put(record.Id, record);
+
+                        currentLine++;
+                        int percentComplete = (int)((float)currentLine / (float)totalLines * 100);
+                        worker.ReportProgress(percentComplete);
+
+                        if (worker.CancellationPending)
+                        {
+                            e.Cancel = true;
+
+                            return null;
+                        }
                     }
                 }
             }
+            worker.ReportProgress(100);
 
+            using (StreamWriter writer = new("newdata.txt"))
+            {
+                foreach (Movie m in movieList)
+                {
+                    if (m.TitleType == "movie")
+                    {
+                        string line = m.Id + "\t" + m.TitleType + "\t" + m.PrimaryTitle + "\t" + m.StartYear + "\t" +
+                            m.RuntimeMinutes + "\t" + m.Genres;
+
+                        writer.WriteLine(line);
+                    }
+                }
+            }
             return movieList;
         }
     }
--- FilmterWPF/ProgressBar.xaml.cs
+++ FilmterWPF/ProgressBar.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DataStructures.Hashing;
 using DataStructures.List;
 using FilmterWPF.Data;
+using FilmterWPF.IO;
 using System;
 using System.ComponentModel;
 using System.IO;
@@ -18,6 +19,8 @@ namespace FilmterWPF
 
         public SinglyLinkedList<BasicMovie> MovieList { get; set; }
         public SeparateChainingHashMap<string, BasicMovie> MovieMap { get; set; }
+        public int EntryCount { get; set; }
+        public int EntryTotal { get; set; }
 
         public ProgressBar(string path)
         {
@@ -73,193 +76,13 @@ namespace FilmterWPF
         private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             pbStatus.Value = e.ProgressPercentage;
-        }
-
-        /// <summary>
-        /// Helper method to determine the total number of entries in the movie data file.
-        /// Used for the percentage complete on the progress bar.
-        /// </summary>
-        /// <returns>The number of entries in the data file.</returns>
-        private int TotalEntriesHelper()
-        {
-            using StreamReader r = new(path);
-            int i = 0;
-            while (r.ReadLine() != null) { i++; }
-            return i;
+            
         }
 
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            e.Result = ReadBasicMovieFile(e);
+            e.Result = FileReader.ReadBasicMovieFile(path, e, worker);
         }
-
-        public SinglyLinkedList<BasicMovie> ReadBasicMovieFile(DoWorkEventArgs e)
-        {
-            bool firstLine = true;
-            SinglyLinkedList<BasicMovie> movieList = new();
-
-            int totalLines = TotalEntriesHelper();
-            //int totalLines = 100000;
-            int currentLine = 0;
-
-            using (StreamReader reader = File.OpenText(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null && currentLine < totalLines)
-                {
-                    if (firstLine)
-                    {
-                        firstLine = false;
-                    }
-                    else
-                    {
-                        string[] values = line.Split('\t');
-                        string id = values[0];
-                        string title = values[2];
-
-                        int? year;
-                        if (Int32.TryParse(values[3], out int temp))
-                        {
-                            year = temp;
-                        }
-                        else
-                        {
-                            //year = null;
-                            continue;
-                        }
-
-                        int? runTimeMinutes;
-                        if (int.TryParse(values[4], out temp))
-                        {
-                            runTimeMinutes = temp;
-                        }
-                        else
-                        {
-                            //runTimeMinutes = null;
-                            continue;
-                        }
-
-                        string genres = values[5];
-
-                        BasicMovie record = new(id, title, year, runTimeMinutes, genres);
-
-                        movieList.AddLast(record);
-                        MovieMap.Put(record.Id, record);
-
-                        currentLine++;
-                        int percentComplete = (int)((float)currentLine / (float)totalLines * 100);
-                        worker.ReportProgress(percentComplete);
-
-                        if (worker.CancellationPending)
-                        {
-                            e.Cancel = true;
-
-                            return null;
-                        }
-                    }
-                }
-            }
-            worker.ReportProgress(100);
-            return movieList;
-        }
-
-        /*
-        public SinglyLinkedList<Movie> ReadFullMovieFile(DoWorkEventArgs e)
-        {
-            SinglyLinkedList<Movie> movieList = new();
-            MovieMap = new();
-            bool firstLine = true;
-
-            int totalLines = TotalLines();
-            //int totalLines = 10000;
-            int currentLine = 0;            
-
-            using (StreamReader reader = File.OpenText(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null && currentLine < totalLines)
-                {
-                    if (firstLine)
-                    {
-                        firstLine = false;
-                    }
-                    else
-                    {
-                        string[] values = line.Split('\t');
-                        string id = values[0];
-                        string titleType = values[1];
-                        string primaryTitle = values[2];
-                        string originalTitle = values[3];
-                        bool isAdult = (values[4] == "1");
-                        int? startYear;
-                        if (Int32.TryParse(values[5], out int temp))
-                        {
-                            startYear = temp;
-                        }
-                        else
-                        {
-                            startYear = null;
-                        }
-
-                        int? endYear;
-                        if (int.TryParse(values[6], out temp))
-                        {
-                            endYear = temp;
-                        }
-                        else
-                        {
-                            endYear = null;
-                        }
-
-                        int? runTimeMinutes;
-                        if (int.TryParse(values[7], out temp))
-                        {
-                            runTimeMinutes = temp;
-                        }
-                        else
-                        {
-                            runTimeMinutes = null;
-                        }
-
-                        string genres = values[8];
-
-                        Movie record = new(id, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runTimeMinutes, genres);
-
-                        movieList.AddLast(record);
-                        //MovieMap.Put(record.Id, record);
-                        
-                        currentLine++;
-                        int percentComplete = (int)((float)currentLine / (float)totalLines * 100);
-                        worker.ReportProgress(percentComplete);
-
-                        if (worker.CancellationPending)
-                        {
-                            e.Cancel = true;
-
-                            return null;
-                        }
-                    }
-                }
-            }
-            worker.ReportProgress(100);
-
-            using (StreamWriter writer = new("newdata.txt"))
-            {
-                foreach (Movie m in movieList)
-                {
-                    if (m.TitleType == "movie")
-                    {
-                        string line = m.Id + "\t" + m.TitleType + "\t" + m.PrimaryTitle + "\t" + m.StartYear + "\t" +
-                            m.RuntimeMinutes + "\t" + m.Genres;
-
-                        writer.WriteLine(line);
-                    }
-                }
-            }
-
-            return movieList;
-        }
-        */
 
         /// <summary>
         /// Handled the OKButton being clicked.
