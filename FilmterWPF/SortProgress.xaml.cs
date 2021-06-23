3883b5c64f83d6788539469b31a0905fd95dee98
Zach Samuels <wzsamuels@gmail.com>
2021-06-22 20:12:18 -0400

    Finished logic to filter and sort by all conditions.

--- FilmterWPF/SortProgress.xaml.cs
+++ FilmterWPF/SortProgress.xaml.cs
@@ -7,7 +7,6 @@ using DataStructures.Sorter;
 using DataStructures.Hashing;
 using static FilmterWPF.MainWindow;
 using System.Collections.ObjectModel;
-using System.Collections.Generic;
 
 namespace FilmterWPF
 {
@@ -18,24 +17,31 @@ namespace FilmterWPF
     {
         private BackgroundWorker worker;
         private SortInfo sortInfo;
+        private FilterInfo filterInfo;
         public ObservableCollection<BasicMovie> MovieList;
         public IMap<string, BasicMovie> MovieMap { get; set; }
-        private ObservableCollection<BasicMovie> movieCollection;
+        private readonly ObservableCollection<BasicMovie> moviesToSort;
+        private TimeSpan sortTimeSpan;
+        private TimeSpan buildMapTimeSpan;
 
-        public SortProgress(SortInfo sortInfo, ObservableCollection<BasicMovie> movieCollection)
+        public SortProgress(SortInfo sortInfo, FilterInfo filterInfo, ObservableCollection<BasicMovie> moviesToSort)
         {
             InitializeComponent();
+
             OkButton.IsEnabled = false;
             CancelButton.IsEnabled = true;
             this.sortInfo = sortInfo;
-            this.movieCollection = movieCollection;
+            this.filterInfo = filterInfo;
+            this.moviesToSort = moviesToSort;           
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             worker = new();
+            worker.WorkerReportsProgress = true;
             worker.WorkerSupportsCancellation = true;
             worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
             worker.RunWorkerCompleted += work_WorkerCompleted;
 
             worker.RunWorkerAsync();
@@ -57,55 +63,75 @@ namespace FilmterWPF
             }
             else
             {
-                DialogResult = true;
                 MovieList = (ObservableCollection<BasicMovie>)e.Result;
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    pbStatus.IsIndeterminate = false;
+                    pbStatus.Value = 100;
+                    LoadingText.Text = $"Building the map took: {buildMapTimeSpan.TotalSeconds:F5} seconds.\n" +
+                    $"Sorting took: {sortTimeSpan.TotalSeconds:F5} seconds.\n";
+                    
+                }));
             }
-        }     
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbStatus.Value = e.ProgressPercentage;
+        }
 
         private void worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            BackgroundWorker worker = sender as BackgroundWorker;
             e.Result = SortAndFilter(e);
         }
 
         public ObservableCollection<BasicMovie> SortAndFilter(DoWorkEventArgs e)
         {
-            //int totalSteps = (int)((float)currentLine / (float)totalLines * 100);
-
-            AbstractComparisonSorter<BasicMovie> sorter;
-            
+            AbstractComparisonSorter<BasicMovie> sorter = null;                    
             IMap<string, BasicMovie> movieMap = null;
 
+            
 
-            // Build map using data type
-            Dispatcher.Invoke(new Action(() =>
-            {
-                LoadingText.Text = "Building map...";
-            }));
-
-            if (sortInfo.dataType == DataType.LinearHash)
+            //if (mw.linearHashRadio.IsChecked == true)
+            if(sortInfo.dataType == DataType.LinearHash)
             {
                 movieMap = new LinearProbingHashMap<string, BasicMovie>();
-            }
-
-            foreach (BasicMovie movie in movieCollection)
+            }       
+            else if(sortInfo.dataType == DataType.UnorderedArrayMap)
             {
-                _ = movieMap.Put(movie.Id, movie);
+                movieMap = new UnorderedArrayMap<string, BasicMovie>();
             }
-
+            else if (sortInfo.dataType == DataType.SeparateChaining)
+            {
+                movieMap = new SeparateChainingHashMap<string, BasicMovie>();
+            }
+            else if (sortInfo.dataType == DataType.SearchTableMap)
+            {
+                movieMap = new SearchTableMap<string, BasicMovie>();
+            }
 
             // Check for sorting algorithm
             if (sortInfo.sortingAlgorithm == SortingAlgorithm.BubbleSort)
             {
                 sorter = new BubbleSorter<BasicMovie>();
-
             }
-            else
+            else if(sortInfo.sortingAlgorithm == SortingAlgorithm.MergeSort)
             {
-                sorter = null;
+                sorter = new MergeSorter<BasicMovie>();
             }
-
-            // Check for filtering
+            else if (sortInfo.sortingAlgorithm == SortingAlgorithm.InsertionSort)
+            {
+                sorter = new InsertionSorter<BasicMovie>();
+            }
+            else if (sortInfo.sortingAlgorithm == SortingAlgorithm.QuickSort)
+            {
+                sorter = new QuickSorter<BasicMovie>();
+            }
+            else if (sortInfo.sortingAlgorithm == SortingAlgorithm.SelectionSort)
+            {
+                sorter = new SelectionSorter<BasicMovie>();
+            }
 
             // Check for sort by
             if (sortInfo.sortBy == SortBy.Year)
@@ -117,9 +143,85 @@ namespace FilmterWPF
                 }
                 else
                 {
-
+                    sorter.SetComparator(BasicMovie.SortYearDescending());
                 }
             }
+            else if(sortInfo.sortBy == SortBy.Title)
+            {
+                if(sortInfo.ascending)
+                {
+                    sorter.SetComparator(BasicMovie.SortTitleAscending());
+                }
+                else 
+                {
+                    sorter.SetComparator(BasicMovie.SortTitleDescending());
+                }
+            }
+            else if(sortInfo.sortBy == SortBy.RunTimeMinutes)
+            {
+                if(sortInfo.ascending)
+                {
+                    sorter.SetComparator(BasicMovie.SortRunTimeAscending());
+                }
+                else
+                {
+                    sorter.SetComparator(BasicMovie.SortRunTimeDescending());
+                }
+            }
+            else
+            {
+                if(sortInfo.ascending)
+                {
+                    sorter.SetComparator(BasicMovie.SortGenresAscending());
+                }
+                else 
+                {
+                    sorter.SetComparator(BasicMovie.SortGenresDescending());
+                }
+            }
+
+            // Build map using data type
+            Dispatcher.Invoke(new Action(() =>
+            {
+                LoadingText.Text = "Building map...";
+            }));
+
+            float currentCount = 0;
+            float totalCount = moviesToSort.Count;
+            DateTime beginMapTime = DateTime.Now;            
+            foreach (BasicMovie movie in moviesToSort)
+            {
+                bool titleMatch = true;
+                bool yearMatch = true;
+                bool runTimeMatch = true;
+                bool genreMatch = true;
+                if(!string.IsNullOrEmpty(filterInfo.title) && !filterInfo.title.Equals(movie.Title)) 
+                {
+                    titleMatch = false;
+                }
+                if (!string.IsNullOrEmpty(filterInfo.year) && !filterInfo.year.Equals(movie.Year.ToString()))
+                {
+                    yearMatch = false;
+                }
+                if (!string.IsNullOrEmpty(filterInfo.runTime) && !filterInfo.runTime.Equals(movie.RunTimeMinutes.ToString()))
+                {
+                    runTimeMatch = false;
+                }
+                if (!string.IsNullOrEmpty(filterInfo.genre) && !filterInfo.genre.Equals(movie.Genres))
+                {
+                    genreMatch = false;
+                }
+
+                if(titleMatch && yearMatch && runTimeMatch && genreMatch)
+                {
+                    _ = movieMap.Put(movie.Id, movie);
+                }
+
+                currentCount++;
+                worker.ReportProgress((int)(currentCount/totalCount * 100));
+            }
+            DateTime endMapTime = DateTime.Now;
+            buildMapTimeSpan = endMapTime - beginMapTime;
 
             Dispatcher.Invoke(new Action(() =>
             {
@@ -137,24 +239,34 @@ namespace FilmterWPF
 
             Dispatcher.Invoke(new Action(() =>
             {
+                pbStatus.Value = 0;
+                pbStatus.IsIndeterminate = true;
+            }));
+
+            Dispatcher.Invoke(new Action(() =>
+            {
                 LoadingText.Text = "Sorting...";
             }));
 
+            DateTime beginSortTime = DateTime.Now;
             sorter.Sort(movieArray);
+            DateTime endSortTime = DateTime.Now;
+
+            sortTimeSpan = endSortTime - beginSortTime;
 
             return new ObservableCollection<BasicMovie>(movieArray);
         }
 
         private void Ok_Button_Click(object sender, RoutedEventArgs e)
         {
+            DialogResult = true;
             Close();
         }
 
         private void Cancel_Button_Click(object sender, RoutedEventArgs e)
         {
-            CancelButton.IsEnabled = false;
-            worker.CancelAsync();
-            LoadingText.Text = "Cancelling...";
+            DialogResult = false;
+            Close();
         }
     }
 }
