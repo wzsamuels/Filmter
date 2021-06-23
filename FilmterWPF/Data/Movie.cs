aa29eb34d74aa9b5f65adea61d6695c92d8604ed
Zach Samuels <wzsamuels@gmail.com>
2021-06-22 20:04:19 -0400

    Completed BasicMovie Comparer classes for sorting all fields.

--- FilmterWPF/Data/Movie.cs
+++ FilmterWPF/Data/Movie.cs
@@ -55,8 +55,8 @@ namespace FilmterWPF.Data
 
             public int Compare(BasicMovie x, BasicMovie y)
             {
-                BasicMovie movie1 = (BasicMovie)x;
-                BasicMovie movie2 = (BasicMovie)y;
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
 
                 if(movie1.Year < movie2.Year)
                 {
@@ -73,9 +73,161 @@ namespace FilmterWPF.Data
             }
         }
 
+        private class SortYearDescendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                if (movie1.Year > movie2.Year)
+                {
+                    return -1;
+                }
+                if (movie1.Year < movie2.Year)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        private class SortTitleAscendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                return movie1.Title.CompareTo(movie2.Title);
+            }
+        }
+
+        private class SortTitleDescendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                return movie1.Title.CompareTo(movie2.Title) * -1;
+            }
+        }
+
+        private class SortRunTimeAscendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                if (movie1.RunTimeMinutes < movie2.RunTimeMinutes)
+                {
+                    return -1;
+                }
+                else if (movie1.RunTimeMinutes > movie2.RunTimeMinutes)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        private class SortRunTimeDescendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                if (movie1.RunTimeMinutes > movie2.RunTimeMinutes)
+                {
+                    return -1;
+                }
+                else if (movie1.RunTimeMinutes < movie2.RunTimeMinutes)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        private class SortGenresAscendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                return movie1.Genres.CompareTo(movie2.Genres);
+            }
+        }
+
+        private class SortGenresDescendingHelper : IComparer<BasicMovie>
+        {
+
+            public int Compare(BasicMovie x, BasicMovie y)
+            {
+                BasicMovie movie1 = x;
+                BasicMovie movie2 = y;
+
+                return movie1.Genres.CompareTo(movie2.Genres) * -1;
+            }
+        }
+
         public static IComparer<BasicMovie> SortYearAscending()
         {
             return new SortYearAscendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortYearDescending()
+        {
+            return new SortYearDescendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortTitleAscending()
+        {
+            return new SortTitleAscendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortTitleDescending()
+        {
+            return new SortTitleDescendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortRunTimeAscending()
+        {
+            return new SortRunTimeAscendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortRunTimeDescending()
+        {
+            return new SortRunTimeDescendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortGenresAscending()
+        {
+            return new SortGenresAscendingHelper();
+        }
+
+        public static IComparer<BasicMovie> SortGenresDescending()
+        {
+            return new SortGenresDescendingHelper();
         }
 
         public int CompareTo(object obj)
